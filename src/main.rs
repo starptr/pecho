da879b8ea0246ad793db67e5d07d0f4e4020cc62
Yuto Nishida <starptr@users.noreply.github.com>
2021-03-05 22:54:10 -0800

    Fix args

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+#[allow(dead_code)]
+#[allow(unused_imports)]
+use std::env;
+use colored::*;
+use clap::{Arg, App, SubCommand};
+
 fn main() {
-    println!("Hello, world!");
+
+    let args: Vec<String> = env::args().collect();
+    println!("{:?}", args);
+
+    let arg_matches = App::new("pecho")
+        .version("0.0.1")
+        .author("Yuto Nishida")
+        .about("Painted echo, or echo with easy colors.")
+        .arg(Arg::with_name("input")
+            .help("The string to print")
+            .index(1))
+        .arg(Arg::with_name("newline")
+            .short("n")
+            .help("No newline at the end."))
+        .arg(Arg::with_name("truecolor")
+            .help("Hex color in xxxxxx format.")
+            .short("t")
+            .long("truecolor")
+            .takes_value(true)
+            .value_name("hex"))
+        .get_matches();
 }
