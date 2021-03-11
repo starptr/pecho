625f63c9dad39f68d0e8929278c862c001341446
Yuto Nishida <starptr@users.noreply.github.com>
2021-03-10 23:06:07 -0800

    Support escaped character codes

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ use colored::*;
 use lazy_static::*;
 use regex::Regex;
 use std::env;
+use std::char;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -74,6 +75,8 @@ fn main() {
         input
     };
 
+    print!("escapes handled: {}", std_print_string);
+
     if let Some(hex) = arg_matches.value_of("truecolor") {
         println!("{}", is_valid_hex(&hex));
     }
@@ -84,11 +87,48 @@ fn replace_std_escapes(input: String, mut no_trailing_newline: bool) -> String {
     let mut formatted: Vec<char> = Vec::new();
     let mut it = input.chars().peekable();
     while let Some(pt) = it.next() {
+        // Handle escaped chars
         if pt == '\\' && it.peek() != None {
             let pt2 = it.next().unwrap();
             if pt2 == '0' || pt2 == 'x' || pt2 == 'u' || pt2 == 'U' {
+                // Handle character code formats
                 let radix = if pt2 == '0' { 8 } else { 16 };
-                let numerals = String::new();
+                let (mini, maxi) = match pt2 {
+                    '0' => (2,3),
+                    'x' => (1,3),
+                    'u' => (4,4),
+                    'U' => (8,8),
+                    _ => panic!(),
+                };
+                // Buffer in the next <=maxi digits
+                let mut numerals: Vec<char> = Vec::new();
+                while numerals.len() < maxi {
+                    let c = it.peek();
+                    if c == None {
+                        // There is no next char
+                        break;
+                    } else {
+                        let c = c.unwrap();
+                        // Check c is a valid digit
+                        let d = c.to_digit(radix);
+                        if d == None {
+                            // c isn't a valid digit
+                            break;
+                        } else {
+                            numerals.push(*c);
+                            it.next();
+                        }
+                    }
+                }
+                // Append character to formatted if the length is correct. Otherwise, append
+                // numerals directly
+                if numerals.len() >= mini {
+                    let numerals: String = numerals.into_iter().collect();
+                    let code_point = u32::from_str_radix(&numerals, radix).unwrap();
+                    formatted.push(char::from_u32(code_point).unwrap());
+                } else {
+                    formatted.append(&mut numerals);
+                }
             } else {
                 formatted.push(match pt2 {
                     'a' => '\x07',
@@ -115,6 +155,11 @@ fn replace_std_escapes(input: String, mut no_trailing_newline: bool) -> String {
         formatted.push('\n');
     }
     formatted.into_iter().collect()
+}
+
+fn append_string_to_vec(vec: &mut Vec<char>, s: String) {
+    let mut vec_2: Vec<char> = s.chars().collect();
+    vec.append(&mut vec_2);
 }
 
 fn is_valid_hex(s: &str) -> bool {
